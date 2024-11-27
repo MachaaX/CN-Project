1f850c052620d3f7b468fa54115f39fb8d7d303b
Pavan Kumar <49985218+MachaaX@users.noreply.github.com>
2024-11-27 12:36:44 -0500

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 import os
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'type your secret key here'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'type_your_secret_key_here'
     SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI') or 'mysql+pymysql://root:admin@localhost/healthcare_db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOAD_FOLDER = 'static/uploads'
