d1b86ef16ea1cf51336846e2b94ede8ed2c33326
Pavan Kumar <49985218+MachaaX@users.noreply.github.com>
2024-11-27 06:35:34 -0500

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'your_secret_key_here'
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:admin@localhost/healthcare_db'
+    # SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:admin@localhost/healthcare_db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOAD_FOLDER = 'static/uploads'
     MAX_CONTENT_LENGTH = 16 * 1024 * 1024  # 16MB max file size
